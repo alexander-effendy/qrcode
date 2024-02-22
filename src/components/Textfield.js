608730a4cb900be9daf5ae9ee9512b7833644d75
alexander-effendy <88591194+alexander-effendy@users.noreply.github.com>
2024-02-22 21:13:32 +1100

    finish styling

--- src/components/Textfield.js
+++ src/components/Textfield.js
@@ -1,43 +1,26 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import QRCode from 'qrcode';
-
 import TextField from '@mui/material/TextField';
 
-export default function BasicTextFields() {
-
-  const [username, setUsername] = React.useState(''); // initial empty input
-  const [src, setSrc] = React.useState('');
-
-  const generate = () => {
-    QRCode.toDataURL(`https://github.com/${username}`).then(setSrc)
-  }
-
+export default function BasicTextFields({ value, onChange }) {
   return (
     <Box
       component="form"
       sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
+        '& > :not(style)': { m: 1, width: '97%' },
       }}
       noValidate
       autoComplete="off"
     >
-      <TextField 
+      <TextField
+      	sx={{ width: '100%' }}
+
         id="outlined-basic" 
         label="username" 
-        variant="outlined"
-        onChange={(e) => setUsername(e.target.value)}
+        variant="outlined" 
+        value={value}
+        onChange={onChange}
       />
-      <Button 
-        variant="text"
-        sx={{ background: 'lightblue' }}
-        onClick={generate}
-      >
-        generate
-      </Button>
-
-      <img alt='qrcode' src={src} />
     </Box>
   );
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,19 +2,116 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Box, Button } from '@mui/material';
 
+import QRCode from 'qrcode';
 import TextField from '../components/Textfield';
+
+import { typography } from '@mui/system';
 
 const Home = () => {
   const navigate = useNavigate();
+
+	const [username, setUsername] = React.useState(''); // initial empty input
+  const [src, setSrc] = React.useState('');
+  const [QRCodeInput, setQRCodeInput] = React.useState(false);
+
+	const generate = () => {
+    // check if the textfield is empty
+    if (username.length === 0) {
+      alert('Please enter something');
+      setQRCodeInput(false);
+    } else {
+      QRCode.toDataURL(`https://github.com/${username}`).then(setSrc);
+      setQRCodeInput(true);
+    }
+  }
+
 	return (
-		<Box>
-			This is home
-			<Button
-				onClick={() => navigate('/About')}
-			>About</Button>
-			<br/>
-			Enter a github username
-			<TextField />
+		<Box
+			sx={{
+				background: 'lightgrey',
+				padding: '10%',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<Box
+				sx={{ 
+					border: '1px solid black', 
+					borderRadius: '20px',
+					display: 'flex' ,
+					padding: '5%',
+					backgroundColor: 'white'
+				}}
+			>
+				{/* box pertama */}
+				<Box
+					sx={{ 
+						padding: '5%',
+						display: 'flex',
+						flexDirection: 'column',
+						height: '50%',
+
+					}}
+				>
+					{/* This is home */}
+					{/* <Button
+						onClick={() => navigate('/About')}
+					>About</Button> */}
+					<typography style={{ fontWeight: 'bold', fontSize: '2em' }}>
+						Generate QR Code for Github User
+					</typography>
+					
+					<Box sx={{ backgroundColor: '' }}>
+						<TextField
+							id="outlined-basic" 
+							label="username" 
+							variant="outlined"
+							onChange={(e) => {
+								setUsername(e.target.value);
+								console.log(username);
+							}}
+						/>
+
+						<Button 
+							variant="text"
+							sx={{ 
+								backgroundColor: 'lightblue', 
+								color: 'black', 
+								border: '1px solid black', 
+								width: '97%',
+								marginLeft: '2%'
+							}}
+							onClick={generate}
+						>
+							generate
+						</Button>
+					</Box>
+					
+					{/*  */}
+					{/*  */}
+					
+				</Box>
+
+				{/* box kedua */}
+				<Box
+					sx={{
+						padding: '1%',
+						display: 'flex',
+						justifyContent: 'center',
+						alignItems: 'center'
+					}}
+				>
+					{QRCodeInput && <img alt='qrcode' src={src} style={{ height: '80%'}} />}
+					{QRCodeInput || 
+						<Box>
+							Cibai
+						</Box>
+					}
+				</Box>
+			</Box>
+			
+				
 		</Box>
 		
 	)
